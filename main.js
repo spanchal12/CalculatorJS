319781fc8b22b2ed61ed20368d462c5b20f3dcb7
Siddhi Panchal <siddhipanchal@Siddhis-MBP.attlocal.net>
2019-10-02 16:10:12 -0700

    finished basic calc

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-//main javascript file 
+//main javascript file
+function calc(){
+  var a = parseInt(document.querySelector("#value1").value);
+  var b = parseInt(document.querySelector("#value2").value);
+
+  var op = document.querySelector("#operator").value;
+
+  var calculate;
+
+  if (op == "add") {
+    calculate = a + b;
+  } else if (op == "min"){
+    calculate = a - b;
+  } else if (op == "div"){
+    calculate = a / b;
+  } else if (op == "mul"){
+    calculate = a * b;
+  }
+
+  //console.log(calculate);
+  document.querySelector("#result").innerHTML = calculate;
+
+
+} //function calc()
